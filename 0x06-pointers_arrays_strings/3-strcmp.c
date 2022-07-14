fa7542d9d8133cf9d1bb61674f0dface5678a45a
chanford87 <hermannchanford@yahoo.fr>
2022-07-14 05:07:11 -0700

    3-strc

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.c"
+#include <string>
+/**
+* _strcmp -> for comparing purpose
+* @s1: param1
+* @s2: param2
+* Return: integer
+*/
+int _strcmp(char *s1, char *s2);
+{
+	strcmp(s1, s2);
+	return (dest);
+}
