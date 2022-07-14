fcdac8681f7f607d39921880f736b3104ef3150d
chanford87 <hermannchanford@yahoo.fr>
2022-07-14 04:34:47 -0700

    2-strnc1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,10 +5,10 @@
 * @dest: param1
 * @src: param2
 * @n: param3
-* Return:string
+* Return: string
 */
 char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
-	return (desr);
+	return (dest);
 }
