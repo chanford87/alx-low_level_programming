44c22337f7c0b173e98878f2cb93188745a9648b
chanford87 <hermannchanford@yahoo.fr>
2022-07-14 02:20:24 -0700

    fix11

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _strcat -> this is a function strcat
- * @dest:first param
- * @src:second param
- * Return: a string
- */
-char *_strcat(char *dest, char *src)
+* _strat -> this is a funcion strcat
+* @dest: first param
+* @src: second param
+* Return: a string
+*/
+char *_strcat(char *dest, char *src);
 {
 	int len = 0, i;
 
 	while (dest[len])
 		len++;
 
-	for (i = 0; src[i] != '\0; i++)
+	for (i = 0; src[i] != 0; i++)
 	{
 		dest[len] = src[i];
 		len += 1;
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ second pointer,;array and strings
