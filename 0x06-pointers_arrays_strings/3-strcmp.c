35ae35fe6254b4839cd37753f83e00a147dfc0d6
chanford87 <hermannchanford@yahoo.fr>
2022-07-15 11:09:34 -0700

    fix3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,5 @@
-#include "main.c"
-#include <string>
+#include "main.h"
+
 /**
 * _strcmp -> for comparing purpose
 * @s1: param1
@@ -8,9 +8,13 @@
 */
 int _strcmp(char *s1, char *s2);
 {
-	int result;
+	int i;
 
-	result = strcmp(*s1, *s2);
-	return (result);
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
 }
 
