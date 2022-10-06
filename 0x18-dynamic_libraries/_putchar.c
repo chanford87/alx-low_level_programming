81e187648ded4e128f90b43c214ea54f859195bd
chanford87 <hermannchanford@yahoo.fr>
2022-10-06 12:30:46 -0700

    fix file

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#inclure <unistd.h>
+
+/**
+ * _putchar - écrit le caractère c à stdout
+ * @c : Le personnage à imprimer
+ *
+ * Retour: Sur le succès 1.
+ * En cas d’erreur, -1 est renvoyé et errno est défini de manière appropriée.
+ */
+int _putchar(char c)
+{
+	retour (écriture(1, &c, 1));
+}
